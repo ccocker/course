78f58c709020c27a57e0d6d883b9e8c14b86bcc2
rodneycockerrmit <rodneyian.cocker@rmit.edu.au>
2023-11-23 15:53:53 +1100

    Updated to correctly place events in Monday column

--- src/app/course-schedule/course-schedule.component.ts
+++ src/app/course-schedule/course-schedule.component.ts
@@ -22,33 +22,141 @@ import { ColorService } from './services/color.service';
 })
 export class CourseScheduleComponent {
   schedule: ScheduleEvent[];
-  timeSlots: string[];
+  timeSlots: { start: string, end: string }[] = [];
+  private occupiedColumnsByTime: Record<string, number> = {};
+
+
+  currentColumn: number = 2;
 
   constructor(
     private scheduleService: ScheduleService,
     private colorService: ColorService
   ) {}
 
+
+
+// Helper function to check if two events overlap
+eventsOverlap(event1: ScheduleEvent, event2: ScheduleEvent): boolean {
+  const start1 = this.convertTimeToMinutes(event1.timeSlot.split(' - ')[0]);
+  const end1 = this.convertTimeToMinutes(event1.timeSlot.split(' - ')[1]);
+  const start2 = this.convertTimeToMinutes(event2.timeSlot.split(' - ')[0]);
+  const end2 = this.convertTimeToMinutes(event2.timeSlot.split(' - ')[1]);
+
+  return start1 < end2 && end1 > start2;
+}
+
+// Helper function to sort events by day and start time
+sortEventsByDayAndTime(schedule: ScheduleEvent[]): ScheduleEvent[] {
+  return schedule.sort((a, b) => {
+    if (a.day === b.day) {
+      return this.convertTimeToMinutes(a.timeSlot.split(' - ')[0]) -
+             this.convertTimeToMinutes(b.timeSlot.split(' - ')[0]);
+    }
+    return a.day.localeCompare(b.day);
+  });
+}
+
+assignColumnsToEvents() {
+  // Sort events by day and start time to compare overlapping times
+  const sortedSchedule = this.sortEventsByDayAndTime(this.schedule);
+  let currentColumn = 2;
+
+  sortedSchedule.forEach((event, index, sortedSchedule) => {
+    if (index > 0) { // Skip the first event as it has nothing to compare with
+      const previousEvent = sortedSchedule[index - 1];
+      const previousEventEnd = this.convertTimeToMinutes(previousEvent.timeSlot.split(' - ')[1]);
+      const currentEventStart = this.convertTimeToMinutes(event.timeSlot.split(' - ')[0]);
+
+      // Check for overlap
+      if (currentEventStart < previousEventEnd) {
+        // If overlapping, increment the column
+        currentColumn++;
+      } else {
+        // If not overlapping, reset the column to 2
+        currentColumn = 2;
+      }
+    }
+
+    // Assign the current column to the event
+    event.gridColumnStart = currentColumn;
+    event.gridColumnEnd = currentColumn; // Assuming events take up only one column
+  });
+}
+
+assignRowsToEvents() {
+  // Sort events by day and start time to compare overlapping times
+  const sortedSchedule = this.sortEventsByDayAndTime(this.schedule);
+
+  sortedSchedule.forEach(event => {
+    // Convert start and end times to grid rows
+    const startRow = this.convertTimeToGridRow(event.timeSlot.split(' - ')[0]);
+    const endRow = this.convertTimeToGridRow(event.timeSlot.split(' - ')[1]);
+
+    // Assign the start and end rows to the event
+    event.gridRowStart = startRow;
+    event.gridRowEnd = endRow; // endRow is inclusive in CSS Grid
+  });
+}
+
+convertTimeToGridRow(time: string): number {
+  // Assuming grid starts at 08:00 as row 2, each half-hour block is a new row
+  const baseTime = new Date(0, 0, 0, 8, 0); // Base time corresponding to row 2
+  const timeParts = time.split(':');
+  const eventTime = new Date(0, 0, 0, parseInt(timeParts[0]), parseInt(timeParts[1]));
+  const diffMinutes = (eventTime.getTime() - baseTime.getTime()) / (1000 * 60);
+  return 1 + (diffMinutes / 30); // Calculate row based on 30-minute increments
+}
   ngOnInit() {
-    this.schedule = this.scheduleService.getSchedule();
-    this.timeSlots = this.scheduleService.getTimeSlots();
+  this.schedule = this.scheduleService.getSchedule();
+  this.assignColumnsToEvents();
+  this.assignRowsToEvents();
+  this.generateTimeSlots();
 
-    const colorConfig = {
-      BC1: '#FFA500',
-      BC2: '#008000',
-      WBC: '#0000ff',
-      // Add more mappings as needed
-    };
+  const colorConfig = {
+    BC1: '#FFA500',
+    BC2: '#008000',
+    WBC: '#0000ff',
+    // Add more mappings as needed
+  };
 
-    // Call the addColorsToObjects function after getting the schedule data
-    this.schedule = this.colorService.addColorsToObjects(
-      this.schedule,
-      colorConfig
-    );
+  // Apply color configuration
+  this.schedule = this.colorService.addColorsToObjects(this.schedule, colorConfig);
 
-    console.log(this.schedule);
-    console.log(this.timeSlots);
-  }
+  console.log(this.schedule);
+  console.log(this.timeSlots);
+}
+
+
+
+sortEventsByStartTime(schedule: ScheduleEvent[]): ScheduleEvent[] {
+  return schedule.sort((a, b) => {
+    return this.convertTimeToMinutes(a.timeSlot.split(' - ')[0]) -
+           this.convertTimeToMinutes(b.timeSlot.split(' - ')[0]);
+  });
+}
+
+calculateColumnForEvent(event: ScheduleEvent, schedule: ScheduleEvent[]): number {
+  // Start with column 2 as per the previous logic
+  let column = 2;
+
+  // Keep checking and incrementing the column until you find one where the event doesn't overlap with any other event
+  let overlapExists;
+  do {
+    overlapExists = schedule.some(e => {
+      return e.day === event.day &&
+             e.gridColumnStart === column &&
+             this.eventsOverlap(event, e);
+    });
+
+    if (overlapExists) {
+      // If an overlap is found, try the next column
+      column++;
+    }
+  } while (overlapExists);
+
+  // Return the column where no overlap exists
+  return column;
+}
 
   // Calculate the row span for each event based on the time duration
   calculateRowSpan(event: any): number {
@@ -62,4 +170,55 @@ export class CourseScheduleComponent {
     const rowSpan = totalMinutes / 30; // Assuming each row represents 30 minutes
     return rowSpan;
   }
+
+  generateTimeSlots() {
+    let startTime = new Date(2023, 0, 1, 8, 30); // Starting at 8:30
+    for (let i = 0; i < 20; i++) { // Assuming you want 20 slots of 30 minutes
+      let endTime = new Date(startTime.getTime() + 30 * 60000); // 30 minutes later
+      this.timeSlots.push({
+        start: this.formatTime(startTime),
+        end: this.formatTime(endTime)
+      });
+      startTime = endTime; // Set start time for the next slot
+    }
+  }
+
+  formatTime(date: Date): string {
+    return date.toLocaleTimeString(navigator.language, {
+      hour: '2-digit',
+      minute:'2-digit',
+      hour12: false
+    }).replace(/:\d{2}\s/, ''); // Remove seconds and AM/PM for clarity
+  }
+
+  convertTimeToMinutes(time: string): number {
+  const [hours, minutes] = time.split(':').map(Number);
+  return hours * 60 + minutes;
+}
+
+
+
+getGridRow(eventTime: string, isStart: boolean): number {
+  const eventTimeMinutes = this.convertTimeToMinutes(eventTime);
+
+  // Find the timeslot index for either the start time or the end time
+  const slotIndex = this.timeSlots.findIndex(slot => {
+    const startTimeMinutes = this.convertTimeToMinutes(slot.start);
+    const endTimeMinutes = this.convertTimeToMinutes(slot.end);
+    return isStart ? eventTimeMinutes >= startTimeMinutes : eventTimeMinutes < endTimeMinutes;
+  });
+
+  // Adjust for the header row and the fact that grid rows are 1-based
+  return slotIndex + 2;
+}
+
+findIndexOfCurrentEvent(currentEvent: ScheduleEvent): number {
+  // Assuming 'schedule' is the array of ScheduleEvent objects
+  return this.schedule.findIndex(event => event.class === currentEvent.class);
+}
+
+
+
+
+
 }
--- src/app/course-schedule/services/schedule.service.ts
+++ src/app/course-schedule/services/schedule.service.ts
@@ -1,439 +1,117 @@
 import { Injectable } from '@angular/core';
 
 export interface ScheduleEvent {
-  day: string;
-  roomNumber: string;
-  timeSlot: string;
-  class: string;
-  lecturer: string;
-  capacity: number;
-  tutors?: string[];
-  color?: string;
-  gridColumnStart?: number;
-  gridColumnEnd?: number;
-  gridRowStart?: number;
-  gridRowEnd?: number;
+	day: string;
+	roomNumber: string;
+	timeSlot: string;
+	class: string;
+	lecturer: string;
+	capacity: number;
+	tutors?: string[];
+	color?: string;
+	gridColumnStart?: number;
+	gridColumnEnd?: number;
+	gridRowStart?: number;
+	gridRowEnd?: number;
 }
 
 interface TimeSlotEventGroup {
-  timeSlot: string;
-  events: ScheduleEvent[];
+	timeSlot: string;
+	events: ScheduleEvent[];
 }
 
 @Injectable({
-  providedIn: 'root',
+	providedIn: 'root'
 })
 export class ScheduleService {
-  private scheduleData: ScheduleEvent[] = [
-    {
-      day: 'Monday',
-      roomNumber: '012.10.004',
-      timeSlot: '08:30 - 10:30',
-      class: 'BC1-G1-1',
-      lecturer: 'Henry Cavill',
-      capacity: 120,
-    },
-    {
-      day: 'Monday',
-      roomNumber: '012.10.005',
-      timeSlot: '08:30 - 10:30',
-      class: 'BC1-G2-1',
-      lecturer: 'Henry Cavill',
-      capacity: 150,
-    },
-    {
-      day: 'Monday',
-      roomNumber: '012.10.004',
-      timeSlot: '10:30 - 12:30',
-      class: 'BC1-G3-1',
-      lecturer: 'Henry Cavill',
-      capacity: 120,
-    },
-    {
-      day: 'Monday',
-      roomNumber: '012.10.005',
-      timeSlot: '10:30 - 12:30',
-      class: 'BC1-G4-1',
-      lecturer: 'Henry Cavill',
-      capacity: 150,
-    },
-    {
-      day: 'Monday',
-      roomNumber: '012.10.004',
-      timeSlot: '12:30 - 14:30',
-      class: 'BC1-G5-1',
-      lecturer: 'Henry Cavill',
-      capacity: 120,
-    },
-    {
-      day: 'Monday',
-      roomNumber: '012.10.005',
-      timeSlot: '12:30 - 14:30',
-      class: 'BC1-G6-1',
-      lecturer: 'Henry Cavill',
-      capacity: 150,
-    },
-    {
-      day: 'Monday',
-      roomNumber: '012.10.004',
-      timeSlot: '14:30 - 16:30',
-      class: 'WBC-G1-1',
-      lecturer: 'Henry Cavill',
-      capacity: 120,
-    },
-    {
-      day: 'Monday',
-      roomNumber: '012.10.005',
-      timeSlot: '14:30 - 16:30',
-      class: 'BC2-G2-1',
-      lecturer: 'Henry Cavill',
-      capacity: 150,
-    },
-    {
-      day: 'Monday',
-      roomNumber: '012.10.004',
-      timeSlot: '16:30 - 18:30',
-      class: 'BC1-7-1',
-      lecturer: 'Henry Cavill',
-      capacity: 120,
-    },
-    {
-      day: 'Monday',
-      roomNumber: '012.10.005',
-      timeSlot: '16:30 - 18:30',
-      class: 'BC2-G2-1',
-      lecturer: 'Henry Cavill',
-      capacity: 150,
-    },
-    {
-      day: 'Tuesday',
-      roomNumber: '012.10.004',
-      timeSlot: '08:30 - 10:30',
-      class: 'BC1-G1-2',
-      lecturer: 'Henry Cavill',
-      capacity: 120,
-    },
-    {
-      day: 'Tuesday',
-      roomNumber: '012.10.005',
-      timeSlot: '08:30 - 10:30',
-      class: 'BC1-G2-2',
-      lecturer: 'Henry Cavill',
-      capacity: 150,
-    },
-    {
-      day: 'Tuesday',
-      roomNumber: '012.10.004',
-      timeSlot: '10:30 - 12:30',
-      class: 'BC1-G3-2',
-      lecturer: 'Henry Cavill',
-      capacity: 120,
-    },
-    {
-      day: 'Tuesday',
-      roomNumber: '012.10.005',
-      timeSlot: '10:30 - 12:30',
-      class: 'BC1-G4-2',
-      lecturer: 'Henry Cavill',
-      capacity: 150,
-    },
-    {
-      day: 'Tuesday',
-      roomNumber: '012.10.005',
-      timeSlot: '09:30 - 11:30',
-      class: 'WBC-2-1',
-      lecturer: 'Henry Cavill',
-      capacity: 120,
-    },
-    {
-      day: 'Tuesday',
-      roomNumber: '012.10.004',
-      timeSlot: '12:30 - 14:30',
-      class: 'BC1-G5-1',
-      lecturer: 'Henry Cavill',
-      capacity: 120,
-    },
-    {
-      day: 'Tuesday',
-      roomNumber: '012.10.005',
-      timeSlot: '12:30 - 14:30',
-      class: 'BC2-G1-1',
-      lecturer: 'Henry Cavill',
-      capacity: 120,
-    },
-    {
-      day: 'Tuesday',
-      roomNumber: '012.10.004',
-      timeSlot: '14:30 - 16:30',
-      class: 'WBC-1-2',
-      lecturer: 'Henry Cavill',
-      capacity: 120,
-    },
-    {
-      day: 'Tuesday',
-      roomNumber: '012.10.005',
-      timeSlot: '14:30 - 16:30',
-      class: 'BC2-G1-2',
-      lecturer: 'Henry Cavill',
-      capacity: 120,
-    },
-    {
-      day: 'Tuesday',
-      roomNumber: '012.10.004',
-      timeSlot: '16:30 - 18:30',
-      class: 'BC1-G7-2',
-      lecturer: 'Henry Cavill',
-      capacity: 120,
-    },
-    {
-      day: 'Tuesday',
-      roomNumber: '012.10.005',
-      timeSlot: '16:30 - 18:30',
-      class: 'BC2-G2-2',
-      lecturer: 'Henry Cavill',
-      capacity: 120,
-    },
-    {
-      day: 'Wednesday',
-      roomNumber: '012.10.004',
-      timeSlot: '08:30 - 10:30',
-      class: 'BC1-G1-A',
-      lecturer: 'Henry Cavill',
-      capacity: 120,
-    },
-    {
-      day: 'Wednesday',
-      roomNumber: '012.10.005',
-      timeSlot: '08:30 - 10:30',
-      class: 'BC1-G2-A',
-      lecturer: 'Henry Cavill',
-      capacity: 150,
-    },
-    {
-      day: 'Wednesday',
-      roomNumber: '012.10.004',
-      timeSlot: '10:30 - 12:30',
-      class: 'BC1-G3-A',
-      lecturer: 'Henry Cavill',
-      capacity: 120,
-    },
-    {
-      day: 'Wednesday',
-      roomNumber: '012.10.005',
-      timeSlot: '10:30 - 12:30',
-      class: 'BC1-G4-A',
-      lecturer: 'Henry Cavill',
-      capacity: 150,
-    },
-    {
-      day: 'Wednesday',
-      roomNumber: '012.10.004',
-      timeSlot: '12:30 - 14:30',
-      class: 'BC1-G5-A',
-      lecturer: 'Henry Cavill',
-      capacity: 120,
-    },
-    {
-      day: 'Wednesday',
-      roomNumber: '012.10.005',
-      timeSlot: '12:30 - 14:30',
-      class: 'BC1-G6-A',
-      lecturer: 'Henry Cavill',
-      capacity: 150,
-    },
-    {
-      day: 'Wednesday',
-      roomNumber: '012.10.004',
-      timeSlot: '14:30 - 16:30',
-      class: 'BC2-G2-3',
-      lecturer: 'Henry Cavill',
-      capacity: 120,
-    },
-    {
-      day: 'Wednesday',
-      roomNumber: '012.10.005',
-      timeSlot: '14:30 - 16:30',
-      class: 'BC1-G7-A',
-      lecturer: 'Henry Cavill',
-      capacity: 150,
-    },
-    {
-      day: 'Wednesday',
-      roomNumber: '012.10.004',
-      timeSlot: '16:30 - 18:30',
-      class: 'BC2-G2-3',
-      lecturer: 'Henry Cavill',
-      capacity: 120,
-    },
-    {
-      day: 'Thursday',
-      roomNumber: '012.10.004',
-      timeSlot: '09:30 - 11:30',
-      class: 'WBC-G2-3',
-      lecturer: 'Henry Cavill',
-      capacity: 120,
-    },
-    {
-      day: 'Thursday',
-      roomNumber: '012.10.004',
-      timeSlot: '14:30 - 16:30',
-      class: 'BC2-G1-A',
-      lecturer: 'Henry Cavill',
-      capacity: 120,
-    },
-    {
-      day: 'Thursday',
-      roomNumber: '012.10.005',
-      timeSlot: '14:30 - 16:30',
-      class: 'WBC-G1-3',
-      lecturer: 'Henry Cavill',
-      capacity: 150,
-    },
-    {
-      day: 'Thursday',
-      roomNumber: '012.10.004',
-      timeSlot: '16:30 - 18:30',
-      class: 'BC2-G2-A',
-      lecturer: 'Henry Cavill',
-      capacity: 120,
-    },
-    {
-      day: 'Friday',
-      roomNumber: '012.10.004',
-      timeSlot: '08:30 - 10:30',
-      class: 'BC1-G1-3',
-      lecturer: 'Henry Cavill',
-      capacity: 120,
-    },
-    {
-      day: 'Friday',
-      roomNumber: '012.10.005',
-      timeSlot: '08:30 - 10:30',
-      class: 'BC1-G2-3',
-      lecturer: 'Henry Cavill',
-      capacity: 150,
-    },
-    {
-      day: 'Friday',
-      roomNumber: '012.10.004',
-      timeSlot: '10:30 - 12:30',
-      class: 'BC1-G3-3',
-      lecturer: 'Henry Cavill',
-      capacity: 120,
-    },
-    {
-      day: 'Friday',
-      roomNumber: '012.10.005',
-      timeSlot: '10:30 - 12:30',
-      class: 'BC1-G4-3',
-      lecturer: 'Henry Cavill',
-      capacity: 150,
-    },
-    {
-      day: 'Friday',
-      roomNumber: '014.09.023',
-      timeSlot: '08:30 - 10:30',
-      class: 'WBC-G2-A',
-      lecturer: 'Henry Cavill',
-      capacity: 60,
-    },
-    {
-      day: 'Friday',
-      roomNumber: '012.10.004',
-      timeSlot: '12:30 - 14:30',
-      class: 'BC1-G5-3',
-      lecturer: 'Henry Cavill',
-      capacity: 120,
-    },
-    {
-      day: 'Friday',
-      roomNumber: '012.10.005',
-      timeSlot: '12:30 - 14:30',
-      class: 'BC2-G6-3',
-      lecturer: 'Henry Cavill',
-      capacity: 150,
-    },
-    {
-      day: 'Friday',
-      roomNumber: '012.10.004',
-      timeSlot: '14:30 - 16:30',
-      class: 'BC1-G7-3',
-      lecturer: 'Henry Cavill',
-      capacity: 120,
-    },
-    {
-      day: 'Friday',
-      roomNumber: '012.10.005',
-      timeSlot: '14:30 - 16:30',
-      class: 'WBC-G1-A',
-      lecturer: 'Henry Cavill',
-      capacity: 150,
-    },
+	private scheduleData: ScheduleEvent[] = [
+		{
+			day: 'Monday',
+			roomNumber: '012.10.004',
+			timeSlot: '08:30 - 10:30',
+			class: 'BC1-G1-1',
+			lecturer: 'Henry Cavill',
+			capacity: 120
+		},
+		{
+			day: 'Monday',
+			roomNumber: '012.10.005',
+			timeSlot: '08:30 - 10:30',
+			class: 'BC1-G2-1',
+			lecturer: 'Henry Cavill',
+			capacity: 150
+		},
+		{
+			day: 'Monday',
+			roomNumber: '014.09.023',
+			timeSlot: '09:30 - 11:30',
+			class: 'WBC-G2-1',
+			lecturer: 'Henry Cavill',
+			capacity: 60
+		},
+		{
+			day: 'Monday',
+			roomNumber: '014.09.023',
+			timeSlot: '11:30 - 12:00',
+			class: 'WBC-G2-1',
+			lecturer: 'Henry Cavill',
+			capacity: 60
+		},
   ];
+	constructor() {}
 
-  constructor() {}
+	getEventsByTimeSlot(): TimeSlotEventGroup[] {
+		const groups = new Map<string, ScheduleEvent[]>();
 
-  getEventsByTimeSlot(): TimeSlotEventGroup[] {
-    const groups = new Map<string, ScheduleEvent[]>();
+		this.scheduleData.forEach((event) => {
+			const group = groups.get(event.timeSlot) || [];
+			group.push(event);
+			groups.set(event.timeSlot, group);
+		});
 
-    this.scheduleData.forEach((event) => {
-      const group = groups.get(event.timeSlot) || [];
-      group.push(event);
-      groups.set(event.timeSlot, group);
-    });
+		return Array.from(groups, ([timeSlot, events]) => ({ timeSlot, events }));
+	}
 
-    return Array.from(groups, ([timeSlot, events]) => ({ timeSlot, events }));
-  }
+	calculateGridPosition(event: ScheduleEvent): ScheduleEvent {
+		const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
+		const [startTime, endTime] = event.timeSlot.split(' - ');
 
-  calculateGridPosition(event: ScheduleEvent): ScheduleEvent {
-    const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
-    const [startTime, endTime] = event.timeSlot.split(' - ');
+		const dayIndex = days.indexOf(event.day) + 2; // +2 because of the header and the first column for times
+		const startRow = this.calculateRow(startTime);
+		const endRow = this.calculateRow(endTime);
 
-    const dayIndex = days.indexOf(event.day) + 2; // +2 because of the header and the first column for times
-    const startRow = this.calculateRow(startTime);
-    const endRow = this.calculateRow(endTime);
+		return {
+			...event,
+			gridColumnStart: dayIndex,
+			gridColumnEnd: dayIndex + 1,
+			gridRowStart: startRow,
+			gridRowEnd: endRow
+		};
+	}
 
-    return {
-      ...event,
-      gridColumnStart: dayIndex,
-      gridColumnEnd: dayIndex + 1,
-      gridRowStart: startRow,
-      gridRowEnd: endRow,
-    };
-  }
+	private calculateRow(time: string): number {
+		const [hours, minutes] = time.split(':').map(Number);
+		// This calculates the correct row, starting at 2 for the first time slot (8:00 AM)
+		return (hours - 8) * 2 + (minutes === 30 ? 1 : 0) + 2;
+	}
 
-  private calculateRow(time: string): number {
-    const [hours, minutes] = time.split(':').map(Number);
-    // This calculates the correct row, starting at 2 for the first time slot (8:00 AM)
-    return (hours - 8) * 2 + (minutes === 30 ? 1 : 0) + 2;
-  }
+	private calculateTutors(capacity: number): string[] {
+		// Subtract 30 from the capacity, divide by 30, and round up
+		const numberOfTutors = Math.ceil((capacity - 30) / 30);
+		// Create an array of tutor names based on the number calculated
+		return Array.from({ length: numberOfTutors }, (_, index) => `Tutor ${index + 1}`);
+	}
 
-  private calculateTutors(capacity: number): string[] {
-    // Subtract 30 from the capacity, divide by 30, and round up
-    const numberOfTutors = Math.ceil((capacity - 30) / 30);
-    // Create an array of tutor names based on the number calculated
-    return Array.from(
-      { length: numberOfTutors },
-      (_, index) => `Tutor ${index + 1}`
-    );
-  }
+	getSchedule() {
+    return this.scheduleData;
+	}
 
-  getSchedule() {
-    return this.scheduleData.map((event) => {
-      const tutors = this.calculateTutors(event.capacity);
-      return {
-        ...this.calculateGridPosition(event),
-        tutors, // Append the tutors array to each event
-      };
-    });
-  }
-
-  getTimeSlots() {
-    const slots = [];
-    for (let hour = 8; hour <= 18; hour++) {
-      slots.push(`${hour.toString().padStart(2, '0')}:00`);
-      slots.push(`${hour.toString().padStart(2, '0')}:30`);
-    }
-    return slots;
-  }
+	getTimeSlots() {
+		const slots = [];
+		for (let hour = 8; hour <= 18; hour++) {
+			slots.push(`${hour.toString().padStart(2, '0')}:00`);
+			slots.push(`${hour.toString().padStart(2, '0')}:30`);
+		}
+		return slots;
+	}
 }
