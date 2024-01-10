d18133e55ceb83e22402fdd94b99ccb50cd6d474
Craig Cocker <craig.cocker@gmail.com>
2024-01-10 12:16:35 +0400

    Moved data out of course schedules to separate files

---
+++ src/app/miApp/components/course-schedule/data/offering.data.ts
@@ -0,0 +1,44 @@
+export const offering: any[] = [
+  {
+    courseCode: 'BC1',
+    startDate: '2024-03-04',
+    finishDate: '2024-06-30',
+    teachStartDate: '2024-03-04',
+    teachFinishDate: '2024-04-21',
+  },
+  {
+    courseCode: 'BC2',
+    startDate: '2024-03-04',
+    finishDate: '2024-06-30',
+    teachStartDate: '2024-03-04',
+    teachFinishDate: '2024-04-21',
+  },
+  {
+    courseCode: 'WBC',
+    startDate: '2024-03-04',
+    finishDate: '2024-06-30',
+    teachStartDate: '2024-03-04',
+    teachFinishDate: '2024-04-21',
+  },
+  {
+    courseCode: 'PS1',
+    startDate: '2024-03-04',
+    finishDate: '2024-06-30',
+    teachStartDate: '2024-04-22',
+    teachFinishDate: '2024-06-30',
+  },
+  {
+    courseCode: 'PS2',
+    startDate: '2024-03-04',
+    finishDate: '2024-06-30',
+    teachStartDate: '2024-04-22',
+    teachFinishDate: '2024-06-30',
+  },
+  {
+    courseCode: 'WS',
+    startDate: '2024-03-04',
+    finishDate: '2024-06-30',
+    teachStartDate: '2024-04-22',
+    teachFinishDate: '2024-06-30',
+  },
+];
