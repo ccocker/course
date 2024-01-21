aabc2cccfb7a3c29587b5b0ecf516c2a2687b848
rodneycockerrmit <rodneyian.cocker@rmit.edu.au>
2024-01-22 10:55:26 +1100

    Updated allocations to allocate based on priority

--- src/app/miApp/components/course-schedule-allocations/services/allocation.service.ts
+++ src/app/miApp/components/course-schedule-allocations/services/allocation.service.ts
@@ -1,26 +1,26 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from '@angular/core'
 
 type ScheduledClass = {
-  day: string;
-  startTime: string; // Format "HH:mm"
-  endTime: string; // Format "HH:mm"
-  groupNumber: string;
-  id: string;
-};
+  day: string
+  startTime: string // Format "HH:mm"
+  endTime: string // Format "HH:mm"
+  groupNumber: string
+  id: string
+}
 
-const priorityOrder = { H: 1, M: 2, L: 3 };
+const priorityOrder = { H: 1, M: 2, L: 3 }
 
 type Preference = {
-  userId: string;
-  classCode: string;
-  priority: string;
-};
+  userId: string
+  classCode: string
+  priority: string
+}
 
 type Allocation = {
-  userId: string;
-  classCode: string;
-  priority: string;
-};
+  userId: string
+  classCode: string
+  priority: string
+}
 
 @Injectable({
   providedIn: 'root',
@@ -28,96 +28,79 @@ type Allocation = {
 export class AllocationService {
   private scheduledClasses: ScheduledClass[] = [
     // ... your scheduled classes data ...
-  ];
+  ]
 
   constructor() {}
 
   // Additional method to process and output allocations if needed
   public processAllocations(preferences: Preference[]): any {
-    preferences = this.filterPreferences(preferences);
-    const sortedPreferences = this.sortTutorsPreferences(preferences);
-    preferences = this.sortTutorPreferencesByPriority(sortedPreferences);
-    const allocations: Allocation[] = this.allocateClasses(preferences);
-    console.log('Preferences', preferences);
-    console.log('Allocations', allocations);
-    debugger;
+    preferences = this.filterPreferences(preferences)
+    debugger
+    const excludedTutors = [
+      'artemis.rosman@rmit.edu.au',
+      'tomaghan@gmail.com',
+      'myeonghoon18@gmail.com',
+      'sakshamjain2552@gmail.com',
+    ]
 
-    return allocations;
-  }
+    preferences = this.filterOutTutors(preferences, excludedTutors)
+    const sortedPreferences = this.sortTutorsPreferences(preferences)
+    preferences = this.sortTutorPreferencesByPriority(sortedPreferences)
+    const allocations: Allocation[] = this.allocateClasses(preferences)
+    console.log('Preferences', preferences)
+    console.log('Allocations', allocations)
 
-  private isTimeOverlap(
-    startTime1: string,
-    endTime1: string,
-    startTime2: string,
-    endTime2: string
-  ): boolean {
-    return !(endTime1 <= startTime2 || startTime1 >= endTime2);
-  }
-
-  private hasTimeConflict(
-    classToCheck: ScheduledClass,
-    allocatedClasses: ScheduledClass[]
-  ): boolean {
-    return allocatedClasses.some((allocatedClass) => {
-      return (
-        allocatedClass.day === classToCheck.day &&
-        this.isTimeOverlap(
-          allocatedClass.startTime,
-          allocatedClass.endTime,
-          classToCheck.startTime,
-          classToCheck.endTime
-        )
-      );
-    });
+    return allocations
   }
 
   filterPreferences(preferences: Preference[]): Preference[] {
-    return preferences.filter((pref) => pref['priority'] !== 'X');
+    return preferences.filter((pref) => pref['priority'] !== 'X')
   }
 
   public allocateClasses(preferences: Preference[]): Allocation[] {
-    let allocations: Allocation[] = [];
-    let allocatedClasses: ScheduledClass[] = [];
-    let remainingPreferences = [...preferences]; // Create a copy of preferences
-    let allocationMade: boolean;
+    let allocations: Allocation[] = []
+    let allocatedPreferences: Set<string> = new Set() // Track allocated preferences
+    let remainingPreferences = [...preferences]
 
-    do {
-      allocationMade = false; // Reset for each iteration
+    while (remainingPreferences.length > 0) {
+      let tutorsAllocatedThisRound: Set<string> = new Set() // Track tutors allocated in this round
 
-      // Iterate through remaining preferences
       for (let i = 0; i < remainingPreferences.length; i++) {
-        const pref = remainingPreferences[i];
+        const pref = remainingPreferences[i]
 
-        allocations.push({
-          userId: pref.userId,
-          classCode: pref.classCode,
-          priority: pref.priority,
-        });
-        allocationMade = true;
+        // Allocate only if the tutor hasn't been allocated a preference in this round
+        if (!tutorsAllocatedThisRound.has(pref.userId)) {
+          allocations.push({
+            userId: pref.userId,
+            classCode: pref.classCode,
+            priority: pref.priority,
+          })
 
-        // Remove the allocated preference
-        remainingPreferences.splice(i, 1);
-        i--; // Adjust the index since we've modified the array
+          tutorsAllocatedThisRound.add(pref.userId) // Mark tutor as allocated this round
+          allocatedPreferences.add(pref['id']) // Mark preference as allocated
+          remainingPreferences.splice(i, 1) // Remove the allocated preference
+          i-- // Adjust the index since we've modified the array
+        }
       }
-    } while (allocationMade && remainingPreferences.length > 0);
+    }
 
-    return allocations;
+    return allocations
   }
 
   private sortTutorsPreferences(preferences: Preference[]): Preference[] {
     // Create a shallow copy of the array
-    const preferencesCopy = [...preferences];
+    const preferencesCopy = [...preferences]
 
     const preferenceCount = preferencesCopy.reduce((count, pref) => {
-      count[pref.userId] = (count[pref.userId] || 0) + 1;
-      return count;
-    }, {});
+      count[pref.userId] = (count[pref.userId] || 0) + 1
+      return count
+    }, {})
 
     return preferencesCopy.sort((a, b) => {
-      const countDiff = preferenceCount[a.userId] - preferenceCount[b.userId];
-      if (countDiff !== 0) return countDiff;
-      return a.userId.localeCompare(b.userId);
-    });
+      const countDiff = preferenceCount[a.userId] - preferenceCount[b.userId]
+      if (countDiff !== 0) return countDiff
+      return a.userId.localeCompare(b.userId)
+    })
   }
 
   sortTutorPreferencesByPriority(preferences: Preference[]): Preference[] {
@@ -125,11 +108,46 @@ export class AllocationService {
       // Check if same tutor
       if (a.userId === b.userId) {
         // Sort by priority within the same tutor
-        return priorityOrder[a['priority']] - priorityOrder[b['priority']];
+        return priorityOrder[a['priority']] - priorityOrder[b['priority']]
       }
 
       // If different tutors, maintain their current order
-      return 0;
-    });
+      return 0
+    })
+  }
+
+  filterOutTutors(
+    preferences: Preference[],
+    excludedEmails: string[],
+  ): Preference[] {
+    return preferences.filter((pref) => !excludedEmails.includes(pref.userId))
+  }
+
+  private hasTimeConflict(
+    classToCheck: ScheduledClass,
+    allocatedClasses: ScheduledClass[],
+  ): boolean {
+    for (const allocatedClass of allocatedClasses) {
+      if (
+        this.isTimeOverlap(
+          classToCheck.startTime,
+          classToCheck.endTime,
+          allocatedClass.startTime,
+          allocatedClass.endTime,
+        )
+      ) {
+        return true // Time conflict found
+      }
+    }
+    return false
+  }
+
+  private isTimeOverlap(
+    startTime1: string,
+    endTime1: string,
+    startTime2: string,
+    endTime2: string,
+  ): boolean {
+    return !(endTime1 <= startTime2 || startTime1 >= endTime2)
   }
 }
