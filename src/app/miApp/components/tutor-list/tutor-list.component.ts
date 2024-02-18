3a07685ec6f4656ab926930bf6ec2dd7238fb522
rodneycockerrmit <rodneyian.cocker@rmit.edu.au>
2024-02-18 18:06:09 +1100

    Tried to get tutor list

--- src/app/miApp/components/tutor-list/tutor-list.component.ts
+++ src/app/miApp/components/tutor-list/tutor-list.component.ts
@@ -1,20 +1,22 @@
-import { Component, ViewChild, AfterViewInit } from '@angular/core';
-import { Store } from '@ngrx/store';
-import { Observable, Subscription, combineLatest } from 'rxjs';
-import { selectTutorPreferences } from '../course-schedule/store/tutor-preferences/reducers';
-import { tutorPreferencesActions } from '../course-schedule/store/tutor-preferences/actions';
-import { AllocationService } from './services/tutor-list.service';
-import { MatTableDataSource, MatTableModule } from '@angular/material/table';
-import { MatSort, MatSortModule } from '@angular/material/sort';
-import { CommonModule } from '@angular/common';
-import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { courseScheduleActions } from '../course-schedule/store/course-schedules/actions';
-import { selectEntities } from '../course-schedule/store/course-schedules/reducers';
-import { entityActions as peopleActions } from '@miCommon/features/entity/store/actions';
-import { selectEntities as selectPeopleEntities } from '@miCommon/features/entity/store/reducers';
-import { MatExpansionModule } from '@angular/material/expansion'; // Import for expansion module
-import { FirestoreDataService } from '@miCommon/services/firestore.data';
+import { Component, ViewChild, AfterViewInit } from '@angular/core'
+import { Store } from '@ngrx/store'
+import { Observable, Subscription, combineLatest } from 'rxjs'
+import { selectTutorPreferences } from '../course-schedule/store/tutor-preferences/reducers'
+import { tutorPreferencesActions } from '../course-schedule/store/tutor-preferences/actions'
+import { AllocationService } from './services/tutor-list.service'
+import { MatTableDataSource, MatTableModule } from '@angular/material/table'
+import { MatSort, MatSortModule } from '@angular/material/sort'
+import { CommonModule } from '@angular/common'
+import { MatInputModule } from '@angular/material/input'
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { courseScheduleActions } from '../course-schedule/store/course-schedules/actions'
+import { selectEntities } from '../course-schedule/store/course-schedules/reducers'
+import { entityActions as peopleActions } from '@miCommon/features/entity/store/actions'
+import { selectEntities as selectPeopleEntities } from '@miCommon/features/entity/store/reducers'
+import { MatExpansionModule } from '@angular/material/expansion' // Import for expansion module
+import { FirestoreDataService } from '@miCommon/services/firestore.data'
+import { map, filter } from 'rxjs/operators'
+import { selectCurrentUser } from '@miCommon/features/auth/store/reducers'
 
 @Component({
   selector: 'mi-tutor-list',
@@ -31,39 +33,49 @@ import { FirestoreDataService } from '@miCommon/services/firestore.data';
   styleUrls: ['./tutor-list.component.scss'],
 })
 export class TutorListComponent implements AfterViewInit {
-  courseSchedule$: Observable<any[]>;
-  tutorpreferences$: Observable<any[]>;
-  people$: Observable<any[]>;
-  courseScheduleSubscription: Subscription;
-  tutorPreferencesSubscription: Subscription;
-  dataSource: MatTableDataSource<any>;
-  dataSource1: MatTableDataSource<any>;
-  displayedColumns: string[] = ['userId']; // Adjust columns as needed
-  displayedColumns1: string[] = ['classCode', 'tutors'];
-  @ViewChild(MatSort) sort: MatSort;
+  courseSchedule$: Observable<any[]>
+  tutorpreferences$: Observable<any[]>
+  people$: Observable<any[]>
+  courseScheduleSubscription: Subscription
+  tutorPreferencesSubscription: Subscription
+  dataSource: MatTableDataSource<any>
+  dataSource1: MatTableDataSource<any>
+  displayedColumns: string[] = ['userId'] // Adjust columns as needed
+  displayedColumns1: string[] = ['classCode', 'tutors']
+  @ViewChild(MatSort) sort: MatSort
+
+  currentUser$: Observable<any> // Assuming "any" is the type of your user, adjust as needed
+  currentUser: any // Adjust the type as needed
+  public filterValue: string = ''
 
   constructor(
     private store: Store,
     private allocationsService: AllocationService,
-    private firestoreDataService: FirestoreDataService
-  ) {}
+    private firestoreDataService: FirestoreDataService,
+  ) {
+    this.currentUser$ = this.store.select(selectCurrentUser) // Ensure you have a selector for the current user
+  }
 
   ngOnInit() {
+    this.currentUser$.subscribe((user) => {
+      this.currentUser = user
+      this.initializeData()
+    })
     this.store.dispatch(
-      tutorPreferencesActions.getTutorPreferences({ url: 'tutorpreferences' })
-    );
+      tutorPreferencesActions.getTutorPreferences({ url: 'tutorpreferences' }),
+    )
     this.store.dispatch(
-      courseScheduleActions.getCourseSchedules({ url: 'course-schedules' })
-    );
+      courseScheduleActions.getCourseSchedules({ url: 'course-schedules' }),
+    )
     this.store.dispatch(
-      tutorPreferencesActions.getTutorPreferences({ url: 'tutorpreferences' })
-    );
+      tutorPreferencesActions.getTutorPreferences({ url: 'tutorpreferences' }),
+    )
 
-    this.store.dispatch(peopleActions.getEntities({ url: 'people' })); // Corrected this line
+    this.store.dispatch(peopleActions.getEntities({ url: 'people' })) // Corrected this line
 
-    this.tutorpreferences$ = this.store.select(selectTutorPreferences);
-    this.courseSchedule$ = this.store.select(selectEntities); // Corrected this line
-    this.people$ = this.store.select(selectPeopleEntities); // Corrected this line
+    this.tutorpreferences$ = this.store.select(selectTutorPreferences)
+    this.courseSchedule$ = this.store.select(selectEntities) // Corrected this line
+    this.people$ = this.store.select(selectPeopleEntities) // Corrected this line
     this.tutorPreferencesSubscription = combineLatest([
       this.tutorpreferences$,
       this.courseSchedule$,
@@ -72,30 +84,97 @@ export class TutorListComponent implements AfterViewInit {
       if (tutorPreferences && tutorPreferences.length > 0) {
         const allocations = this.allocationsService.processAllocations(
           tutorPreferences,
-          courseSchedule
-        );
-        this.dataSource = new MatTableDataSource(allocations);
-        this.applyFilter({ target: { value: '' } } as any); // Apply the filter to the dataSource
+          courseSchedule,
+        )
+        this.dataSource = new MatTableDataSource(allocations)
+        if (this.currentUser?.displayName === 'Julie Porteous') {
+          this.filterValue = 'BC2'
+          this.filterEmailsForBC2('BC2')
+        }
+        if (this.currentUser?.displayName === 'Edouard Amouroux') {
+          this.filterValue = 'BC1'
+          this.filterEmailsForBC2('BC1')
+        }
+        console.log('Filtered', this.dataSource.data)
+        console.log()
+        /*
+           const henry = new Set(
+          this.dataSource.data
+            .filter((item) => item.classCode.includes('BC2'))
+            .map((item) => item.userId), // Assuming each item has an email field
+        )
+
+        console.log('henry', henry)
+        let henryArray = [...henry]
+        this.dataSource.data = henryArray
+
+        //    this.applyFilter({ target: { value: '' } } as any) // Apply the filter to the dataSource
 
         // this.updateCourseSchedules(allocations, courseSchedule, people);
+        */
       }
-    });
+    })
+  }
+
+  filterEmailsForBC2(course: string) {
+    const emailsForBC2 = new Set(
+      this.dataSource.data
+        .filter((item) => item.classCode && item.classCode.includes(course))
+        .map((item) => item.userId),
+    )
+    this.dataSource.data = Array.from(emailsForBC2)
+  }
+
+  transformDataSource(): void {
+    const userIdsSet = new Set()
+    const filteredData = this.dataSource.data.filter((item) => {
+      if (!userIdsSet.has(item.userId)) {
+        userIdsSet.add(item.userId)
+      }
+      return true
+    })
+
+    // Now, filteredData contains the desired array with unique userIds
+
+    // Update dataSource.data with the filtered array
+    const henry = new Set(filteredData.map((item) => item.userId))
+    let henryArray = [...henry]
+
+    this.dataSource.data = henryArray
+  }
+
+  initializeData() {
+    // Dispatch actions to fetch tutor preferences, course schedules, and people only if currentUser is defined
+    if (this.currentUser) {
+      this.store.dispatch(
+        tutorPreferencesActions.getTutorPreferences({
+          url: 'tutorpreferences',
+        }),
+      )
+      this.store.dispatch(
+        courseScheduleActions.getCourseSchedules({ url: 'course-schedules' }),
+      )
+      this.store.dispatch(peopleActions.getEntities({ url: 'people' }))
+
+      // Now, with the currentUser, you can filter the courses and tutors accordingly
+      this.setupFilteredTutors()
+    }
   }
 
   ngAfterViewInit() {
     // this.dataSource.sort = this.sort;
   }
 
-  applyFilter(event: Event) {
-    const filterValue = (event.target as HTMLInputElement).value;
-    this.dataSource.filter = filterValue.trim().toLowerCase(); // This sets the filter string
+  applyFilter(value: string) {
+    console.log('Event', value)
+    this.dataSource.filter = value.trim().toLowerCase() // This sets the filter string
 
     // Now, to create a Set of userIds based on a condition, filter the dataSource's data array instead
-    const filteredData = this.dataSource.data.filter((item) =>
-      item.yourStringProperty.toLowerCase().includes(this.dataSource.filter)
-    );
-    const userIdSet = new Set(filteredData.map((item) => item.userId));
-    console.log(userIdSet);
+    // const filteredData = this.dataSource.data.filter((item) =>
+    // item.yourStringProperty.toLowerCase().includes(this.dataSource.filter),
+    //)
+    // const userIdSet = new Set(filteredData.map((item) => item.userId))
+    // console.log(userIdSet)
   }
 
   // updateCourseSchedules(allocations, courseSchedules, people) {
@@ -158,14 +237,41 @@ export class TutorListComponent implements AfterViewInit {
         '-' +
         data['leadName'] +
         '-' +
-        data['tutors'];
-    });
-    this.firestoreDataService.uploadBulkData('course-schedules', data, true);
+        data['tutors']
+    })
+    this.firestoreDataService.uploadBulkData('course-schedules', data, true)
+  }
+
+  setupFilteredTutors() {
+    // Combine latest data streams: tutor preferences, course schedules, and people
+    this.tutorPreferencesSubscription = combineLatest([
+      this.tutorpreferences$,
+      this.courseSchedule$,
+      this.people$,
+    ])
+      .pipe(
+        map(([tutorPreferences, courseSchedule, people]) => {
+          // Filter logic here: Use this.currentUser to filter courseSchedule and tutorPreferences
+          // Example:
+          const myCourses = courseSchedule.filter(
+            (c) => c.leadId === this.currentUser.id,
+          )
+          const tutorIds = new Set(myCourses.map((c) => c.tutorId))
+          const tutors = people.filter((p) => tutorIds.has(p.id))
+          return { tutors, myCourses } // Adjust according to your data structure
+        }),
+      )
+      .subscribe(({ tutors, myCourses }) => {
+        // Use this data to set your dataSource or any other properties you need for display
+      })
   }
 
   ngOnDestroy() {
     if (this.tutorPreferencesSubscription) {
-      this.tutorPreferencesSubscription.unsubscribe();
+      this.tutorPreferencesSubscription.unsubscribe()
+    }
+    if (this.tutorPreferencesSubscription) {
+      this.tutorPreferencesSubscription.unsubscribe()
     }
   }
 }
